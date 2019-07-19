18bde270374cfe92c3a3b12406783d034b6790f2
Puneet Saini <puneet29saini@gmail.com>
2019-07-19 18:01:34 +0530

    Added Flattening a linked list

---
+++ GeeksForGeeks/FlatteningALinkList.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    struct Node *bottom;
+};
+
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+}
+
+Node *merge(Node *a, Node *b)
+{
+    if (a == NULL)
+    {
+        return b;
+    }
+    if (b == NULL)
+    {
+        return a;
+    }
+
+    Node *result;
+    if (a->data < b->data)
+    {
+        result = a;
+        result->bottom = merge(a->bottom, b);
+    }
+    else
+    {
+        result = b;
+        result->bottom = merge(a, b->bottom);
+    }
+    return result;
+}
+
+Node *flatten(Node *root)
+{
+    if (root == NULL || root->next == NULL)
+    {
+        return root;
+    }
+    return merge(root, flatten(root->next));
+}
+
+int main(void)
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, flag = 1, flag1 = 1;
+        struct Node *temp = NULL;
+        struct Node *head = NULL;
+        struct Node *pre = NULL;
+        struct Node *tempB = NULL;
+        struct Node *preB = NULL;
+        cin >> n;
+        int work[n];
+        for (int i = 0; i < n; i++)
+            cin >> work[i];
+        for (int i = 0; i < n; i++)
+        {
+            m = work[i];
+            --m;
+            temp = (struct Node *)malloc(sizeof(struct Node));
+            scanf("%d", &temp->data);
+            temp->next = NULL;
+            temp->bottom = NULL;
+            if (flag)
+            {
+                head = temp;
+                pre = temp;
+                flag = 0;
+                flag1 = 1;
+            }
+            else
+            {
+                pre->next = temp;
+                pre = temp;
+                flag1 = 1;
+            }
+            for (int j = 0; j < m; j++)
+            {
+                tempB = (struct Node *)malloc(sizeof(struct Node));
+                scanf("%d", &tempB->data);
+                tempB->next = tempB->bottom = NULL;
+                if (flag1)
+                {
+                    temp->bottom = tempB;
+                    preB = tempB;
+                    flag1 = 0;
+                }
+                else
+                {
+                    preB->bottom = tempB;
+                    preB = tempB;
+                }
+            }
+        }
+        Node *fun = head;
+        Node *fun2 = head;
+        Node *root = flatten(head);
+        printList(root);
+        cout << endl;
+    }
+    return 0;
+}
