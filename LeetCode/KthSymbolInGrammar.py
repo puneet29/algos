f3a037be3c37546aecbf8c5e35da48377902d001
Puneet Saini <puneet29saini@gmail.com>
2020-02-17 19:54:37 +0530

    Optimized KthSymbolInGrammar

--- LeetCode/KthSymbolInGrammar.py
+++ LeetCode/KthSymbolInGrammar.py
@@ -1,9 +1,12 @@
+import math
+
+
 class Solution:
     def kthGrammar(self, N: int, K: int) -> int:
-        a = '0'
-        for i in range(1, N):
-            b = ''
-            for j in a:
-                b += '01' if j == '0' else '10'
-            a = b
-        return a[K-1]
+        if(N == 1 and K == 1):
+            return 0
+        prev = self.kthGrammar(N-1, math.ceil(K/2))
+        if(prev == 0):
+            return 1 if K % 2 == 0 else 0
+        else:
+            return 0 if K % 2 == 0 else 1
