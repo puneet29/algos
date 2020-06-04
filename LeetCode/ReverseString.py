bdd8765a04293df101b3b2568ab45af9cf300a1a
Puneet Saini <puneet29saini@gmail.com>
2020-06-04 16:42:57 +0530

    Added another solution to reverse String

--- LeetCode/ReverseString.py
+++ LeetCode/ReverseString.py
@@ -8,3 +8,12 @@ class Solution:
                 s[left], s[right] = s[right], s[left]
                 helper(left+1, right-1)
         helper(0, len(s)-1)
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s)//2):
+            s[i], s[len(s)-i-1] = s[len(s)-i-1], s[i]
