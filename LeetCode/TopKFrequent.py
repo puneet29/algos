ac8a2871c7e243b724b44ebcb7f374f94f43bc79
puneeter <puneet.saini@quantumblack.com>
2024-12-30 17:16:22 +0530

    Added comment

--- LeetCode/TopKFrequent.py
+++ LeetCode/TopKFrequent.py
@@ -11,5 +11,6 @@ class Solution(object):
         freq = defaultdict(int)
         for num in nums:
             freq[num] += 1
+        # Also look at bucket sort solution (better in theory - O(n))
         freq = sorted(freq, key=lambda x: freq[x], reverse=True)
         return freq[:k]
