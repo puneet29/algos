d18bb04a4f178d35af26f34f922e811450e52d1d
puneeter <puneet.saini@quantumblack.com>
2025-01-03 00:39:45 +0530

    Updated best time to buy and sell stock

--- LeetCode/BestTimeToBuyAndSellStock.py
+++ LeetCode/BestTimeToBuyAndSellStock.py
@@ -4,18 +4,11 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        n = len(prices)
-        if n == 1:
-            return 0
-        left, right = 0, 1
+        min_price = 10001
         max_profit = 0
-        maximum_achievable_profit = max(prices) - min(prices)
-        while right<n:
-            if max_profit == maximum_achievable_profit:
-                break
-            if prices[left] > prices[right]:
-                left = right
-            profit = prices[right] - prices[left]
-            max_profit = max(profit, max_profit)
-            right += 1
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
         return max_profit
