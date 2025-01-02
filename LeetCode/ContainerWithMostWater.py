f8e06e5170ae76b8e136c6dc905984c125486ab7
puneeter <puneet.saini@quantumblack.com>
2025-01-03 00:01:38 +0530

    Update Container with most water

--- LeetCode/ContainerWithMostWater.py
+++ LeetCode/ContainerWithMostWater.py
@@ -4,20 +4,20 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        global_max_volume = -1
+        max_volume = 0
+        max_height = max(height)
         left_idx = 0
-        while left_idx < len(height):
+        right_idx = len(height) - 1
+        while left_idx < right_idx:
             left_wall = height[left_idx]
-            max_volume = global_max_volume
-            right_idx = len(height) - 1
-            while right_idx > left_idx:
-                w = right_idx - left_idx
-                if left_wall * w <= max_volume:
-                    break
-                right_wall = height[right_idx]
-                h = min(right_wall, left_wall)
-                max_volume = max(max_volume, w * h)
+            right_wall = height[right_idx]
+            width = (right_idx - left_idx)
+            area = width * min(left_wall, right_wall)
+            max_volume = max(max_volume, area)
+            if max_height * width <= max_volume:
+                break
+            if left_wall > right_wall:
                 right_idx -= 1
-            global_max_volume = max(global_max_volume, max_volume)
-            left_idx += 1
-        return global_max_volume
+            else:
+                left_idx += 1
+        return max_volume
